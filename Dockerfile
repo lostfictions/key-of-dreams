0af76a203d4aa92097764f2bc893acb21612b241
lostfictions <lostfictions@users.noreply.github.com>
2019-05-30 05:31:17 -0400

    attempting to simplify cron usage

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,10 @@
 FROM node:12.1
-
 MAINTAINER s
 
 WORKDIR /code
 
 # https://stackoverflow.com/questions/37458287/how-to-run-a-cron-job-inside-a-docker-container
 RUN apt-get update && apt-get -y install -qq cron && apt-get clean
-COPY cronrule /etc/cron.d/mastobot
-RUN chmod +x /etc/cron.d/mastobot && crontab /etc/cron.d/mastobot
 
 COPY package.json yarn.lock ./
 RUN yarn install --frozen-lockfile
@@ -17,4 +14,4 @@ RUN yarn build
 
 ENV NODE_ENV=production
 ENV DEBUG=*
-ENTRYPOINT ["sh", "run.sh"]
+ENTRYPOINT ["bash", "run.sh"]
--- run.sh
+++ run.sh
@@ -1,2 +1,23 @@
+#!/bin/bash
+set -Eeuo pipefail
+
+DEFAULT_CRON_RULE='0 */4 * * *'
+
+if [[ -z "${CRON_RULE-}" ]]; then
+    echo "CRON_RULE not set in environment, using default rule ($DEFAULT_CRON_RULE)"
+fi
+
+# convincing cron to redirect stdout/stderr even in a docker container:
+# https://stackoverflow.com/a/41409061
+echo "${CRON_RULE-$DEFAULT_CRON_RULE} yarn --cwd $PWD start > /proc/\$(cat /var/run/crond.pid)/fd/1 2>&1" > /etc/cron.d/mastobot
+# cron wants a newline at the end of a file, too.
+printf "\n" >> /etc/cron.d/mastobot
+chmod +x /etc/cron.d/mastobot
+crontab /etc/cron.d/mastobot
+
+# cron doesn't have access to our container environment variables by default, so
+# we just dump them to /etc/environment, as suggested here:
+# https://stackoverflow.com/a/41938139
+# this may pose some security concerns somehow, caveat emptor, etc etc.
 printenv >> /etc/environment
 cron -f -L 15
