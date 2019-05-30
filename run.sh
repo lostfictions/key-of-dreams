7693f2c65e74413773152412adbe59fed7008728
lostfictions <lostfictions@users.noreply.github.com>
2019-05-30 05:38:02 -0400

    more documentation for run script

--- run.sh
+++ run.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 set -Eeuo pipefail
 
+# every four hours, on the hour
 DEFAULT_CRON_RULE='0 */4 * * *'
 
 if [[ -z "${CRON_RULE-}" ]]; then
     echo "CRON_RULE not set in environment, using default rule ($DEFAULT_CRON_RULE)"
 fi
 
-# convincing cron to redirect stdout/stderr even in a docker container:
-# https://stackoverflow.com/a/41409061
+# the spooky stuff at the end is to convince cron to redirect stdout/stderr even
+# in a docker container: https://stackoverflow.com/a/41409061
 echo "${CRON_RULE-$DEFAULT_CRON_RULE} yarn --cwd $PWD start > /proc/\$(cat /var/run/crond.pid)/fd/1 2>&1" > /etc/cron.d/mastobot
 # cron wants a newline at the end of a file, too.
 printf "\n" >> /etc/cron.d/mastobot
+
 chmod +x /etc/cron.d/mastobot
 crontab /etc/cron.d/mastobot
 
@@ -20,4 +22,7 @@ crontab /etc/cron.d/mastobot
 # https://stackoverflow.com/a/41938139
 # this may pose some security concerns somehow, caveat emptor, etc etc.
 printenv >> /etc/environment
+
+# finally, run cron in the foreground (-f). '-L 15', in debian, also turns on
+# verbose logging: https://manpages.debian.org/stretch/cron/cron.8.en.html
 cron -f -L 15
